6d666387279325b7c666be5de5c5348c0c07acdf
kinxlo <outof5ime.gmail.com>
2020-06-07 23:30:07 +0100

    Removed the look and feel, now the app will run...

--- Elements Prototype 1.0/src/view/AppEntryPoint.java
+++ Elements Prototype 1.0/src/view/AppEntryPoint.java
@@ -10,22 +10,7 @@ public class AppEntryPoint {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				try {
-					UIManager.setLookAndFeel("net.sourceforge.napkinlaf.NapkinLookAndFeel");
-		
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InstantiationException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UnsupportedLookAndFeelException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				
 				runApp();
 			}
 		});
